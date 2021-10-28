982ccfc6c248af6bd824706809a52164209b230c
Abrahamv12 <92050635+Abrahamv12@users.noreply.github.com>
2021-10-28 12:08:05 -0500

    11:04
    Cambios y arreglos del juego

--- Assets/Scripts/ControlEnemigo.cs
+++ Assets/Scripts/ControlEnemigo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlEnemigo : MonoBehaviour
 {
@@ -25,5 +26,13 @@ public class ControlEnemigo : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name); 
+    if (collision.gameObject.name == "Player" || collision.gameObject.name == "Base")
+        {
+            ResetScene();
+        }
+    }
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
